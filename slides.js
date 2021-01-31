209f5773caac867bc8ac885c0eef5d670e5c2382
John Beech <johnbeech.github@mkv25.net>
2021-01-31 12:34:39 +0000

    Rewrite markdown links within slides to link to other presentations

--- slides.js
+++ slides.js
@@ -22,6 +22,36 @@ function startSlides() {
 
   console.log('Params:', params)
   console.log('Started remark slideshow:', slideshow, 'from', sourceUrl)
+
+  slideshow.on('showSlide', rewriteMarkdownLinks)
+}
+
+let rewriteLock = false
+function rewriteMarkdownLinks(slide) {
+  if (rewriteLock) {
+    return
+  }
+  rewriteLock = true
+
+  console.log('Rewriting markdown links to improve slide navigation:')
+  const slidesAreaEl = document.getElementsByClassName('remark-slides-area')[0]
+  const hrefEls = Array.from(slidesAreaEl.getElementsByTagName('a'))
+  const rewrittenUrls = hrefEls.reduce(rewriteMarkdownLink, [])
+  console.log('Found and rewrote:', rewrittenUrls.length, 'slide links in this presentation.')
+}
+
+function rewriteMarkdownLink(list, el) {
+  const pattern = /\/([A-z\d-/]+.md)$/
+  const href = new URL(el.href)
+  const [,markdownPath] = (href.pathname.match(pattern) || [])
+  const rewrittenAlready = href.pathname.includes('?view=')
+  if (markdownPath && !rewrittenAlready) {
+    const slideViewerPath = `?view=${markdownPath}`
+    // console.log('Rewriting', el, markdownPath, 'from', el.href, 'to', slideViewerPath)
+    el.href = slideViewerPath
+    list.push(el)
+  }
+  return list
 }
 
 if (typeof remark !== undefined) {
