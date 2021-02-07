f27dbdf8969a57f9b4e966082c6c182b83bf965e
John Beech <johnbeech.github@mkv25.net>
2021-02-07 02:53:05 +0000

    Change local express server to match hosted website

--- start.js
+++ start.js
@@ -4,13 +4,13 @@ const { name } = require('./package.json')
 const app = express()
 const port = 8080
 
-app.get('/', async (req, res) => {
+app.get('/content/', async (req, res) => {
   const html = await fs.readFile(`${__dirname}/slides.html`, 'utf8')
   res.send(html)
 })
 
-app.use('/', express.static(__dirname))
+app.use('/content/', express.static(__dirname))
 
 app.listen(port, () => {
-  console.log(`${name} server running at: http://localhost:${port}`)
+  console.log(`${name} server running at: http://localhost:${port}/content/`)
 })
