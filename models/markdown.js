6e2cf2ddd3b4128a7bee6ebfb5de8f392ab309b1
John Beech <johnbeech.github@mkv25.net>
2021-09-10 14:31:04 +0100

    Generate JSON models for all markdown files in project

--- models/markdown.js
+++ models/markdown.js
@@ -1,22 +1,22 @@
 /* Run this from the root using: node models/markdown.js */
-const fs = require('fs/promises')
+const { find, clean, make, read, write, position } = require('promise-path')
 const { convertMarkdownToDocument } = require('./utils/markdown2json')
 
 async function convertMarkdownFileToJsonFile(inputpath, outputpath) {
-  const markdown = await fs.readFile(inputpath, 'utf8')
+  const markdown = await read(inputpath, 'utf8')
   const document = convertMarkdownToDocument(markdown)
-  return await fs.writeFile(outputpath, JSON.stringify(document, null, 2), 'utf8')
+  return await write(outputpath, JSON.stringify(document, null, 2), 'utf8')
 }
 
 async function run() {
-  const outputdir = 'models/generated/'
-  const files = [
-    'README.md',
-    'SOCIAL-MEDIA-CHECKLIST.md',
-    'TOPICS.md',
-    'WRITING-GUIDE.md',
-    'WRITING-TEMPLATE.md'
-  ]
+  const root = position(__dirname, '..')
+  const outputdir = root('models/generated/')
+  const files = (await find('**/*.md')).filter(n => !n.includes('node_modules'))
+
+  await clean(outputdir)
+  await make(outputdir)
+
+  console.log('Files', files)
 
   const work = files.map(filepath => {
     const outputname = filepath.toLowerCase().split('.')[0] + '.json'
